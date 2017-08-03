c5bb11298b17f76babc8147c4e71018aad2044e4
ilhamizzul <ilhamizzul@gmail.com>
2017-08-03 15:56:27 +0700

    set hoverable sidebar (example)

--- resources/views/front/sidebar.blade.php
+++ resources/views/front/sidebar.blade.php
@@ -1,10 +1,13 @@
  <!--left-->
 <nav class="navbar navbar-default col-md-2">
-	<div class="collapse navbar-collapse" id="sidebar">
-		<ul class="nav navbar-nav">
+	<div class="collapse navbar-collapse">
+		<ul class="nav navbar-nav" id="sidebar">
         	    @foreach($category as $list)
         	  <li><a href="/article/{{$list->id}}" style="color:white">{{$list->category}}</a></li>
         	    @endforeach
+        	   <li><a>Home</a></li>
+        	   <li><a>Sport</a></li>
+        	   <li><a>Social</a></li>
        	</ul>
     </div>
 </nav><!--/left-->
