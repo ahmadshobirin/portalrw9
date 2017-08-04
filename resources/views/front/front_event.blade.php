94342b573f8c5c33af22586a9bade9f61d44dc6d
ilhamizzul <ilhamizzul@gmail.com>
2017-08-04 11:09:19 +0700

    fixing sidebar problem
    right sidebar

--- resources/views/front/front_event.blade.php
+++ resources/views/front/front_event.blade.php
@@ -9,11 +9,7 @@
 
 
 <ul class="nav flex-column col-md-offset-1 col-md-3 rightSide">
-<<<<<<< HEAD
-	<li class="panel-heading" style="padding:0"><h3></h3></li>
-    <li>
 
-    {{-- <img src="/img/event2.jpg" class="img-responsive" style="width:75%"> --}}
 
 
 
@@ -22,6 +18,14 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-md-12">
+                    	{{-- Search Option --}}
+                    	<div class="input-group">
+						  <input type="text" class="form-control">
+						  <span class="input-group-btn">
+						    <button class="btn btn-default" type="button">Search</button>
+						  </span>
+						</div>
+						<br>
                         <div class="card ">
                             <div class="header">
 		                            <center>
@@ -56,21 +60,8 @@
                     </div>
                 </div>
             </div>
-    </li>
-</ul>
 
 
 
 
 
-=======
-	<div class="input-group col-md-9">
-	  <input type="text" class="form-control">
-	  <span class="input-group-btn">
-	    <button class="btn btn-default" type="button">Search</button>
-	  </span>
-	</div>
-	<li class="panel-heading" style="padding:0"><h3>Event</h3></li>
-    <li><img src="/img/event2.jpg" class="img-responsive" style="width:75%"></li>
-</ul>
->>>>>>> d6d90cc08c9db283053269aa7947fdfe5d2f6c84
