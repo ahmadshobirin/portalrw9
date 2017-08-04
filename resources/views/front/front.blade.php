0e75986c0f2e856ed60a4c0276b402c92445dda5
Smktelkom <hufmalang@gmail.com>
2017-08-04 10:55:07 +0700

    hbd sidebar
    belum fix, mau dikembangkan lagi

--- resources/views/front/front.blade.php
+++ resources/views/front/front.blade.php
@@ -4,7 +4,29 @@
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 		<meta charset="utf-8">
 		<title>title</title>
-		<meta name="generator" content="Bootply" />
+		{{-- {{ asset('css/app.css') }} --}}
+    <!-- Bootstrap core CSS     -->
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" />
+
+    <!-- Animation library for notifications   -->
+    <link href="{{ asset('assets/css/animate.min.css') }}" rel="stylesheet"/>
+
+    <!--  Light Bootstrap Table core CSS    -->
+    <link href="{{ asset('assets/css/light-bootstrap-dashboard.css') }}" rel="stylesheet"/>
+
+
+    <!--  CSS for Demo Purpose, don't include it in your project     -->
+    <link href="{{ asset('assets/css/demo.css') }}" rel="stylesheet" />
+
+
+    <!--     Fonts and icons     -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+      <link href="{{ asset('assets/css/pe-icon-7-stroke.css') }}" rel="stylesheet" />
+
+    {{-- izul --}}
+
+    <meta name="generator" content="Bootply" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 		<link href="/front_layout/css/bootstrap.min.css" rel="stylesheet">
@@ -12,7 +34,9 @@
 			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
 		<link href="/front_layout/css/styles.css" rel="stylesheet">
-	</head>
+	
+
+  </head>
 	<body>
 <nav id="top-nav" class="navbar navbar-default navbar-fixed-top" role="banner">
   <div class="container">
@@ -87,5 +111,29 @@
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
 		<script src="/front_layout/js/bootstrap.min.js"></script>
 		<script src="/front_layout/js/scripts.js"></script>
+
+
+    
+    <!--   Core JS Files   -->
+    <script src="{{ asset('assets/js/jquery-1.10.2.js') }}" type="text/javascript"></script>
+  <script src="{{ asset('assets/js/bootstrap.min.js') }}" type="text/javascript"></script>
+
+  <!--  Checkbox, Radio & Switch Plugins -->
+  <script src="{{ asset('assets/js/bootstrap-checkbox-radio-switch.js') }}"></script>
+
+  <!--  Charts Plugin -->
+  <script src="{{ asset('assets/js/chartist.min.js') }}"></script>
+
+    <!--  Notifications Plugin    -->
+    <script src="{{ asset('assets/js/bootstrap-notify.js') }}"></script>
+
+    <!--  Google Maps Plugin    -->
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+
+    <!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
+  <script src="{{ asset('assets/js/light-bootstrap-dashboard.js') }}"></script>
+
+  <!-- Light Bootstrap Table DEMO methods, don't include it in your project! -->
+  <script src="{{ asset('assets/js/demo.js') }}assets/js/demo.js"></script>
 	</body>
 </html>
--- resources/views/front/front_event.blade.php
+++ resources/views/front/front_event.blade.php
@@ -7,6 +7,56 @@
 </div> --}}<!--/full right-->
 
 <ul class="nav flex-column col-md-offset-1 col-md-3 rightSide">
-	<li class="panel-heading" style="padding:0"><h3>Event</h3></li>
-    <li><img src="/img/event2.jpg" class="img-responsive" style="width:75%"></li>
+	<li class="panel-heading" style="padding:0"><h3></h3></li>
+    <li>
+
+    {{-- <img src="/img/event2.jpg" class="img-responsive" style="width:75%"> --}}
+
+
+
+
+
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card ">
+                            <div class="header">
+		                            <center>
+		                                <h4 class="title">Ulang Tahun Hari Ini</h4>
+		                            </center>
+                            </div>
+                            <div class="content">
+                                <div class="table-full-width">
+                                <table class="table">
+								    <tbody>
+								        <tr>
+								            <th class="col-sm-6"><center>Hari ini</center></th>
+								            <th class="col-sm-6"><center>Besok</center></th>
+								        </tr>
+								        <tr>
+								            <td><center>ultah</center></td>
+								            <td><center>ultah</center></td>
+								        </tr>
+								        <tr>
+								            <td><center>ultah</center></td>
+								            <td><center>ultah</center></td>
+								        </tr>
+								        <tr> 
+								            <td><center>dan 11 orang lainnya</center></td>
+								            <td><center>-</center></td>
+								        </tr>
+								    </tbody>
+								</table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </li>
 </ul>
+
+
+
+
+
