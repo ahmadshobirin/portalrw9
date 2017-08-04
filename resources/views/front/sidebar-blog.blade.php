f0d02cc2f120d83ceb9984e17462eb4ff8445728
ilhamizzul <ilhamizzul@gmail.com>
2017-08-04 14:30:20 +0700

    detail content and next topic sidebar temporary done
    also i make the code more clean and tidy

---
+++ resources/views/front/sidebar-blog.blade.php
@@ -0,0 +1,25 @@
+<ul class="nav flex-column col-md-3 rightSide">
+    <li class="panel-heading" style="padding:0">
+        <h3>Next Post</h3>
+    </li>
+    <li>
+        <div class="thumbnail" id="side-blog">
+            <h4 class="title">Travel Terbaik! eta Terangkanlah~</h4>
+            <img src="/img/traveljinni-logo-icon.png">
+            <div class="caption">
+                <p class="text-muted">POSTED BY IZZUL | 1 JULY 2017</p>
+                <p class="text-left"></p>
+            </div>
+        </div>
+    </li>
+    <li>
+        <div class="thumbnail" id="side-blog">
+            <h4 class="title">Travel Terbaik! eta Terangkanlah~</h4>
+            <img src="/img/traveljinni-logo-icon.png">
+            <div class="caption">
+                <p class="text-muted">POSTED BY IZZUL | 1 JULY 2017</p>
+                <p class="text-left"></p>
+            </div>
+        </div>
+    </li>
+</ul>
