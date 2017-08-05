c392d69de8ba4c650b62c03c6b449808cc99284b
nofitaaaa <nofitamahfudiyah@gmail.com>
2017-08-05 10:35:23 +0700

    modal suda bisa close

--- resources/views/front/front_event.blade.php
+++ resources/views/front/front_event.blade.php
@@ -1,3 +1,25 @@
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" role="dialog">
+<div class="modal-dialog">
+
+  <!-- Modal content-->
+  <div class="modal-content">
+    <div class="modal-header">
+      <button type="button" class="close" data-dismiss="modal">&times;</button>
+      <h4 class="modal-title">Modal Header</h4>
+    </div>
+    <div class="modal-body">
+      <p>Some text in the modal.</p>
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+    </div>
+  </div>
+  
+</div>
+</div>
+
 <ul class="nav flex-column col-md-offset-1 col-md-3 rightSide">
     <div class="container-fluid">
         <div class="row">
@@ -33,27 +55,6 @@
                                                 <td>                               
                                                       <!-- Trigger the modal with a button -->
                                                       <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal"><span class="fa fa-birthday-cake fa-3x"></button>
-
-                                                      <!-- Modal -->
-                                                      <div class="modal fade" id="myModal" role="dialog">
-                                                        <div class="modal-dialog">
-                                                        
-                                                          <!-- Modal content-->
-                                                          <div class="modal-content">
-                                                            <div class="modal-header">
-                                                              <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                                              <h4 class="modal-title">Modal Header</h4>
-                                                            </div>
-                                                            <div class="modal-body">
-                                                              <p>Some text in the modal.</p>
-                                                            </div>
-                                                            <div class="modal-footer">
-                                                              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                                            </div>
-                                                          </div>
-                                                          
-                                                        </div>
-                                                      </div>
                                                 </td>
                                             </tr>
                                             <tr>
