a4712b2845bc6766d03d879e8b88845ba5c6e999
Smktelkom <hufmalang@gmail.com>
2017-08-09 08:41:17 +0700

    revisi link untuk logo portal dan gambar article

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,14 +11,16 @@
 
         @forelse($article as $list)
             <div class="panel panel-default">
-               <div class="panel-thumbnail"><img src="/storage/{{$list->images}}" class="img-responsive" style="width: 100%; max-height: 400px;">
-               </div>
-               <div class="caption panel-body">
-                  <p class="text-muted">POSTED BY ADMIN | {{$list->created_at->diffForHumans()}}</p>
-                  <a href="{{url('article/post/'.$list->slug)}}"> <h3>{{$list->title}}</h3> </a>
-                  <p class="text-left">{{$list->description}}</p>
-                  <span> <a href="#" class="category">{{$list->category}}</a></span>
-               </div>
+                <a href="{{url('article/post/'.$list->slug)}}">
+                  <div class="panel-thumbnail"><img src="/storage/{{$list->images}}" class="img-responsive" style="width: 100%; max-height: 400px;">
+                  </div>
+                </a>
+                  <div class="caption panel-body">
+                    <p class="text-muted">POSTED BY ADMIN | {{$list->created_at->diffForHumans()}}</p>
+                    <a href="{{url('article/post/'.$list->slug)}}"> <h3>{{$list->title}}</h3> </a>
+                    <p class="text-left">{{$list->description}}</p>
+                    <span> <a href="#" class="category">{{$list->category}}</a></span>
+                  </div>
             </div>
           @empty
            <div class="thumbnail">
