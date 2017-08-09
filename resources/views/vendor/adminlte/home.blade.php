c111f0d2a62b15c6f72976d74dd5479c0f016501
nofitaaaa <nofitamahfudiyah@gmail.com>
2017-08-09 09:29:18 +0700

    icon trash admin

--- resources/views/vendor/adminlte/home.blade.php
+++ resources/views/vendor/adminlte/home.blade.php
@@ -58,7 +58,7 @@
         <div class="panel-heading">
             <div class="row">
                 <div class="col-xs-3">
-                    <i class="fa fa-list fa-5x"></i>
+                    <i class="fa fa-trash-o fa-5x"></i>
                 </div>
                 <div class="col-xs-9 text-right">
                     <div class="huge" style="font-size: 40px;">{{$articleTrash}}</div>
