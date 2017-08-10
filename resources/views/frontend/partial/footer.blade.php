d53f1d0f2fb259120168d50231e881160879d9fb
ilhamizzul <ilhamizzul@gmail.com>
2017-08-10 14:05:52 +0700

    footer done :v:

--- resources/views/frontend/partial/footer.blade.php
+++ resources/views/frontend/partial/footer.blade.php
@@ -1,7 +1,19 @@
 
 
 <div id="footer">
-    <div class="col-sm-12">
+	<div class="col-sm-3">
+		<a href="">Tentang Kami</a>
+	</div>
+	<div class="col-sm-3" id="border">
+		<a href="">Manajemen dan redaksi</a>
+	</div>
+	<div class="col-sm-3" id="border">
+		<a href="">Pedoman Media Cyber</a>
+	</div>
+	<div class="col-sm-3" >
+		<a href="">Ketentuan Layanan</a>
+	</div>
+    <div class="col-sm-12" style="padding-top: 10px;">
         <p>
             © Copyright PortalRw9 All Right Reserved
         </p>
