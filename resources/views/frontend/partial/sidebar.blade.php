2de2f7ecb8ba875ee5d02d0d9f227159375822ae
ilhamizzul <ilhamizzul@gmail.com>
2017-08-12 14:37:58 +0700

    temporary finishing mobile responsif

--- resources/views/frontend/partial/sidebar.blade.php
+++ resources/views/frontend/partial/sidebar.blade.php
@@ -5,12 +5,12 @@
       @forelse($article as $list)
 
             <div class="panel-body topik">
-               <div class="col-md-4">
+               <div class="col-md-4 col-lg-4 col-sm-4 col-xs-4">
                   <a href="{{url('article/post/'.$list->slug)}}">
                      <img src="{{URL::asset('/storage/'.$list->images)}}" style="width: 85px; height: 85px;">
                   </a>
                </div>
-               <div class="col-md-8">
+               <div class="col-md-8 col-lg-8 col-sm-8 col-xs-8">
                   <b>
                      <a href="{{url('article/post/'.$list->slug)}}">
                         <p style="font-size: 15px;"> {{$list->title}} </p>
