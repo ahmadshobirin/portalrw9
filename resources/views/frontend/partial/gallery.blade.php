c60be444becbe785940e3a4070f1d5f88cd077ee
ilhamizzul <ilhamizzul@gmail.com>
2017-08-24 11:41:48 +0700

    gallery done

--- resources/views/frontend/partial/gallery.blade.php
+++ resources/views/frontend/partial/gallery.blade.php
@@ -1,4 +1,4 @@
-<div class="panel panel-default side">
+<div class="panel panel-default">
    <div class="panel-heading">
       <h4 style="padding-left: 10px;padding-right: 9px;"> Gallery!</h4>
    </div>
@@ -34,8 +34,38 @@
          </a>
       </div>
       <div class="col-sm-4 col-xs-4 col-md-4 col-lg-4 thumbnail">
-         <a href="{{URL::asset('assets/img/2.jpg')}}" data-type="image" target="_blank" data-lightbox="photo">
-            <img src="{{URL::asset('assets/img/2.jpg')}}" alt="" class="img-responsive">
+         <a href="{{URL::asset('assets/img/8.jpg')}}" data-type="image" target="_blank" data-lightbox="photo">
+            <img src="{{URL::asset('assets/img/8.jpg')}}" alt="" class="img-responsive">
+         </a>
+      </div>
+      <div class="col-sm-4 col-xs-4 col-md-4 col-lg-4 thumbnail">
+         <a href="{{URL::asset('assets/img/9.jpg')}}" data-type="image" target="_blank" data-lightbox="photo">
+            <img src="{{URL::asset('assets/img/9.jpg')}}" alt="" class="img-responsive">
+         </a>
+      </div>
+      <div class="col-sm-4 col-xs-4 col-md-4 col-lg-4 thumbnail">
+         <a href="{{URL::asset('assets/img/10.jpg')}}" data-type="image" target="_blank" data-lightbox="photo">
+            <img src="{{URL::asset('assets/img/10.jpg')}}" alt="" class="img-responsive">
+         </a>
+      </div>
+      <div class="col-sm-4 col-xs-4 col-md-4 col-lg-4 thumbnail">
+         <a href="{{URL::asset('assets/img/11.jpg')}}" data-type="image" target="_blank" data-lightbox="photo">
+            <img src="{{URL::asset('assets/img/11.jpg')}}" alt="" class="img-responsive">
+         </a>
+      </div>
+      <div class="col-sm-4 col-xs-4 col-md-4 col-lg-4 thumbnail">
+         <a href="{{URL::asset('assets/img/12.jpg')}}" data-type="image" target="_blank" data-lightbox="photo">
+            <img src="{{URL::asset('assets/img/12.jpg')}}" alt="" class="img-responsive">
+         </a>
+      </div>
+      <div class="col-sm-4 col-xs-4 col-md-4 col-lg-4 thumbnail">
+         <a href="{{URL::asset('assets/img/13.jpg')}}" data-type="image" target="_blank" data-lightbox="photo">
+            <img src="{{URL::asset('assets/img/13.jpg')}}" alt="" class="img-responsive">
+         </a>
+      </div>
+      <div class="col-sm-4 col-xs-4 col-md-4 col-lg-4 thumbnail">
+         <a href="{{URL::asset('assets/img/7.jpg')}}" data-type="image" target="_blank" data-lightbox="photo">
+            <img src="{{URL::asset('assets/img/7.jpg')}}" alt="" class="img-responsive">
          </a>
       </div>
    </div>
