cf82f46a51a7ad9c62c29e69ffceb43c2992861a
Smktelkom <hufmalang@gmail.com>
2017-08-26 13:12:04 +0700

    waktu waktu telah berlalu

--- resources/views/frontend/main.blade.php
+++ resources/views/frontend/main.blade.php
@@ -104,6 +104,22 @@
               }
          });
         </script>
+        <script>
+          var slideIndex = 0;
+          showSlides();
+
+          function showSlides() {
+              var i;
+              var slides = document.getElementsByClassName("mySlides");
+              for (i = 0; i < slides.length; i++) {
+                  slides[i].style.display = "none"; 
+              }
+              slideIndex++;
+              if (slideIndex> slides.length) {slideIndex = 1} 
+              slides[slideIndex-1].style.display = "block"; 
+              setTimeout(showSlides, 7000); // Change image every 2 seconds
+          }
+        </script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.9.0/js/lightbox-plus-jquery.min.js"></script>
       <!-- END JS -->
    </body>
